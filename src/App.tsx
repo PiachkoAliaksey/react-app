fd88c658c24eeb97570592a311d0a5da72fae019
PiachkoAliaksey <alek17y@mail.ru>
2022-11-09 21:30:37 +0300

    init: base configuration react project with typescript and react

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import{BrowserRouter, Route, Routes,redirect, Navigate} from 'react-router-dom';
+
+
+ function App()  {
+
+    return (
+      <div className='App'>
+        Hello World!
+      </div>
+
+    )
+
+}
+
+export default App;
+
+
