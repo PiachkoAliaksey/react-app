f4e20cc35caae13de2779a68b89c6189455388b7
PiachkoAliaksey <alek17y@mail.ru>
2022-11-11 21:21:26 +0300

    refactor: components react

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import{BrowserRouter, Route, Routes,redirect, Navigate} from 'react-router-dom';
+import './sass/normalize.scss';
+import styles from './sass/style.module.scss';
+import getApiRes from './utils/network';
 
 
  function App()  {
 
     return (
-      <div className='App'>
-        Hello World!
+      <div className={styles.mainWrapper}>
       </div>
 
     )
