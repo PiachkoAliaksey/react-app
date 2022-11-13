da1749a2c63f3da61786439eb8a943a32945f18b
PiachkoAliaksey <alek17y@mail.ru>
2022-11-13 18:26:31 +0300

    add: constants API

---
+++ src/constants/api.tsx
@@ -0,0 +1,12 @@
+export const baseUrl = 'swapi.dev/api/';
+export const apiPeople = 'people';
+export const HTTPS = 'https://';
+export const HTTP = 'http://';
+export const rootPeople = `${HTTPS}${baseUrl}/${apiPeople}`;
+
+//https://starwars-visualguide.com/assets/img/characters/22.jpg
+const rootImg = 'https://starwars-visualguide.com/assets/img/';
+const rootImgPeople = 'characters';
+export const imgExt = '.jpg';
+
+export const urlImgPeople = `${rootImg}${rootImgPeople}`
