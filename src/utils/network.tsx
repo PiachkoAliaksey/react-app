87199df630eec7e957604e57ca4364ff21f381a7
PiachkoAliaksey <alek17y@mail.ru>
2022-11-13 18:28:39 +0300

    add: fuction networks

--- src/utils/network.tsx
+++ src/utils/network.tsx
@@ -1,22 +1,17 @@
-
-const baseUrl = 'https://swapi.dev/api/';
-const rootPeople = `${baseUrl}/people`;
-
 const getApiRes = async (url:string) =>{
 
 try{
 const response = await fetch(url);
 if(!response.ok){
   console.log('Could.error', response.status)
+  return false;
 }
 return await response.json();
 
 }catch(error){
   console.error('Could not fetch');
   return false;
-
 };
 }
 
-getApiRes(rootPeople);
 export default getApiRes;
