7910b07380cda3f39567d543a459d934b3fc1f10
PiachkoAliaksey <alek17y@mail.ru>
2022-11-28 21:46:56 +0300

    add: functional component HomePage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+const HomePage = () => {
+
+  return (
+    <>
+    <main className='home-page'>
+      <h2>Welcome to Universe of Star Wars</h2>
+    </main>
+    </>
+  )
+
+}
+
+export default HomePage;
