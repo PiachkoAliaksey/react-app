0205fc92c86c03411e3596ca10ff10a2120f34df
PiachkoAliaksey <alek17y@mail.ru>
2022-11-28 21:44:58 +0300

    refactor: functional component ErrorMessage

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+const ErrorMessage = () => {
+
+  return (
+    <>
+    <h2 className='errorMessageText'>The dark side of the force has won.<br/>
+    We can't display the data.<br/>
+    Pleas, comeback later
+    </h2>
+    </>
+  )
+
+}
+
+export default ErrorMessage;
