c3ff760a8f04cdb11d1b9ff8bf7702512abac134
PiachkoAliaksey <alek17y@mail.ru>
2022-11-28 21:48:01 +0300

    refactor: component App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
-import{BrowserRouter, Route, Routes,redirect, Navigate} from 'react-router-dom';
+import{BrowserRouter, Route, Routes,redirect, Navigate,Link} from 'react-router-dom';
+import PeoplePage from './pages/PeoplePage';
+import HomePage from './pages/HomePage';
+import Navigation from './components/Navigation';
+
+import routesConfig from './routes/routesConfig';
+
 import './sass/normalize.scss';
 import './sass/style.scss';
-import PeoplePage from './pages/PeoplePage';
 
 
  function App() {
-
     return (
-        <PeoplePage/>
-    )
+      <>
 
+<BrowserRouter>
+      <Navigation/>
+      <Routes>
+        {routesConfig.map((route,id) => (
+          <Route key = {id} path = {route.path} element={<route.component/>}></Route>
+        ))}
+      </Routes>
+</BrowserRouter>
+
+      </>
+    )
 }
 
 export default App;
