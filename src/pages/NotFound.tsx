b7630d6a01a8a9e4367df0689ac07308b940d0f3
PiachkoAliaksey <alek17y@mail.ru>
2022-11-28 21:46:30 +0300

    add: functional component NotFound

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+const NotFound = () => {
+
+  return (
+    <div className='container-not-found'>
+      <h2>404</h2>
+      <h3>Not Found</h3>
+      <img src = 'https://media3.giphy.com/media/33iqmp5ATXT5m/giphy.gif' alt= 'image' />
+
+    </div>
+  )
+
+}
+
+export default NotFound;
