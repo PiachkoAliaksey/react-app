de1614448094ea2cd0c71b27204527dd2ea188b2
PiachkoAliaksey <alek17y@mail.ru>
2022-11-28 21:48:35 +0300

    refactor: functional component PeoplePAge

--- src/pages/PeoplePage.tsx
+++ src/pages/PeoplePage.tsx
@@ -1,45 +1,81 @@
 import React, { Component,useEffect,useState } from 'react';
+
+import ListOfPeople from '../components/ListOfPeople';
+import {withErrorApi} from '../hoc-helpers/withErrorApi';
 import getApiRes from '../utils/network';
-import { rootPeople} from '../constants/api';
 import {getIdOfPeople,getPeopleImg} from '../utils/getId';
+//import {rootPeople} from '../constants/api';
+import {useQueryParam} from '../hooks/useQueryParam';
 
 interface IElementOfObject{
 name: string
 url: string
 }
-const PeoplePage = () => {
+interface IView {
+  setErrorApi:React.Dispatch<React.SetStateAction<boolean>>,
+}
+const PeoplePage:React.FC<IView> = ({setErrorApi}) => {
 
-  const[elements, setElement] = useState([])
+  const[elements, setElement] = useState([]);
+  //const[errorApi, setErrorApi] = useState(false);
+  const[numberPage, setNumberPage] = useState(1);
+  //const rootPeople = `https://swapi.dev/api/people/?page=${numberPage}`
+  //console.log(numberPage);
+  //console.log(rootPeople);
+
+
+
   const getResource = async(url:string) => {
     const body = await getApiRes(url);
-    const peopleList = body.results.map((element: IElementOfObject)=>{
-      const id = getIdOfPeople(element.url);
-      const img = getPeopleImg(id);
-      return {
-        id:id,
-        name:element.name,
-        img:img
+    console.log(body);
+    if(body) {
+      const peopleList = body.results.map((element: IElementOfObject)=>{
+        const id = getIdOfPeople(element.url);
+        const img = getPeopleImg(id);
+        return {
+          id:id,
+          name:element.name,
+          img:img
+        }
       }
+      )
+      setElement(peopleList);
+      setErrorApi(false);
+    } else {
+      setErrorApi(true);
     }
-    )
-    setElement(peopleList)
+
+
   }
 
   useEffect(()=>{
-    getResource(rootPeople);
+    getResource(`https://swapi.dev/api/people/?page=${numberPage}`);
   },[])
+
+  function setHandleCurrentPage(){
+    setNumberPage(numberPage+1);
+    getResource(`https://swapi.dev/api/people/?page=${numberPage}`);
+  }
+
+  function setHandleCurrentPageBack(){
+    numberPage===1?setNumberPage(1):setNumberPage(numberPage-1);
+    getResource(`https://swapi.dev/api/people/?page=${numberPage}`);
+  }
+
+
+
 
   return (
     <>
-    <ul className='listOfElements'>
-      {elements!.map((element:{id:string,name:string,img:string})=> <li key ={element.id} className='elementOfList'>
-        <img className='imgOfElement' src = {element.img} alt = 'img'/>
-        <p>{element.name}</p>
-        </li>)}
-    </ul>
+    <button onClick={() => setHandleCurrentPageBack()}>Previous</button>
+    <button onClick={() =>setHandleCurrentPage()}>Next</button>
+
+    <ListOfPeople list = {elements}/>
     </>
+
   )
 
 }
 
-export default PeoplePage;
+export default withErrorApi(PeoplePage) ;
+//<ListOfPeople list = {elements}/>
