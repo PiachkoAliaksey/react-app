b6e65593131372697039ddd02ef245fcfaddcd29
PiachkoAliaksey <alek17y@mail.ru>
2022-12-12 21:16:06 +0300

    refactor: network

--- src/utils/network.tsx
+++ src/utils/network.tsx
@@ -1,17 +1,23 @@
-const getApiRes = async (url:string) =>{
 
-try{
-const response = await fetch(url);
-if(!response.ok){
-  console.log('Could.error', response.status)
-  return false;
-}
-return await response.json();
+/**
+ * REST API fetch
+ * @param {String} url - url for request
+ * @returns {Promise} - Promise with result of request
+ */
+const getApiRes = async (url: string) => {
 
-}catch(error){
-  console.error('Could not fetch');
-  return false;
-};
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      console.log('Could.error', response.status)
+      return false;
+    }
+    return await response.json();
+
+  } catch (error) {
+    console.error('Could not fetch');
+    return false;
+  };
 }
 
 export default getApiRes;
