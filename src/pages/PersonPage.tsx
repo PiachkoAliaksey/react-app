0b5f43dba58a3ddd749db672ce51fff638b3f4af
PiachkoAliaksey <alek17y@mail.ru>
2023-01-07 16:42:03 +0300

    refactor: code clean and function getCurrentRequest

--- src/pages/PersonPage.tsx
+++ src/pages/PersonPage.tsx
@@ -1,60 +1,79 @@
-import React, { Component,useEffect,useState } from 'react';
-import {useParams} from "react-router-dom";
-import getApiRes from '../utils/network';
+import React, { Component, useEffect, useState, Suspense } from 'react';
+import { useParams } from "react-router-dom";
+import { getApiRes, getCurrentRequest } from '../utils/network';
 import { rootPeople } from '../constants/api';
 import { withErrorApi } from '../hoc-helpers/withErrorApi';
 import { getPeopleImg } from '../utils/getId';
 import PersonInfo from '../components/PersonPage/PersonInfo/PersonInfo';
 import PersonPhoto from '../components/PersonPage/PersonImage/PersonPhoto';
+import PersonLinkBack from '../components/PersonPage/PersonLinkBack/PersonLinkBack';
+import PersonMovies from '../components/PersonPage/PersonMovies/PersonMovies';
+import UiLoadingPerson from '../components/UI/UiLoadingPerson/UiLoadingPerson';
 
+//const PersonMovies = React.lazy(()=> import('../components/PersonPage/PersonMovies/PersonMovies'))
 interface IPerson {
-  title:string, data:string|number
+  title: string, data: string | number
 }[]
 
 interface IPersonPage {
-  setErrorApi:React.Dispatch<React.SetStateAction<boolean>>
+  setErrorApi: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const PersonPage:React.FC<IPersonPage> = ({setErrorApi}) => {
-  const {id} = useParams();
+const PersonPage: React.FC<IPersonPage> = ({ setErrorApi }) => {
+  const { id } = useParams();
 
-  const[person, setPerson] = useState([{title:'', data:''}]);
+  const [person, setPerson] = useState([{ title: '', data: '' }]);
   const [personName, setPersonName] = useState('');
   const [imgPerson, setImgPerson] = useState('');
+  const [personMovies, setPersonMovies] = useState<Array<{ title: string, episode_id: number }>>([]);
+  const [isLoadingMovies, setLoadingMovies] = useState(false);
 
-  useEffect(()=>{
-    const fetchData = async() =>{
-
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoadingMovies(true);
       const data = await getApiRes(`${rootPeople}/${id}/`)
-
-      if(data) {
+      if (data) {
+        console.log(data);
         const image = getPeopleImg(id!);
         setImgPerson(image)
         setPersonName(data.name);
         setPerson([
-          {title:'Height', data:data.height},
-          {title:'Mass', data:data.mass},
-          {title:'Gender', data:data.gender},
-          {title:'Eye color', data:data.eye_color},
-          {title:'Birth year', data:data.birth_year},
+          { title: 'Height', data: data.height },
+          { title: 'Mass', data: data.mass },
+          { title: 'Gender', data: data.gender },
+          { title: 'Eye color', data: data.eye_color },
+          { title: 'Birth year', data: data.birth_year },
         ]);
         //res.films
+        //console.log(data.films)
+        if (data.films.length) {
+          const movies = await getCurrentRequest(data.films);
+          console.log(movies);
+          setLoadingMovies(false);
+          setPersonMovies(movies);
+        }
         setErrorApi(false);
-      }else{
+      } else {
         setErrorApi(true);
       }
     }
     fetchData();
 
-  },[])
+
+  }, [])
 
   return (
     <>
-    <div className='block-person-page'>
-      <h2>Name: {personName}</h2>
-      <img className='imgOfPerson' src = {imgPerson} alt = {personName}/>
-      {person && (<PersonInfo arrPerson = {person} />)}
-    </div>
+      <div className='block-person-page'>
+        <PersonLinkBack />
+        <h2 className='name-of-person'>Name: {personName}</h2>
+        <div className='block-person-properties'>
+          <PersonPhoto imgPerson={imgPerson} personName={personName} />
+          {person && (<PersonInfo arrPerson={person} />)}
+          {isLoadingMovies?<UiLoadingPerson/>:(personMovies && <PersonMovies personMovies={personMovies} />)}
+        </div>
+
+      </div>
     </>
   )
 
