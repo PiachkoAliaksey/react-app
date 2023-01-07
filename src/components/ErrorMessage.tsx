a4ce14468032bd377eea9e6cb52426adbf546b09
PiachkoAliaksey <alek17y@mail.ru>
2023-01-07 16:48:46 +0300

    refactor: clean code

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -3,10 +3,10 @@ const ErrorMessage = () => {
 
   return (
     <>
-    <h2 className='errorMessageText'>The dark side of the force has won.<br/>
-    We can't display the data.<br/>
-    Pleas, comeback later
-    </h2>
+      <h2 className='errorMessageText'>The dark side of the force has won.<br />
+        We can't display the data.<br />
+        Pleas, comeback later
+      </h2>
     </>
   )
 
