427b0f0bd6d825cd4f49be45d0015881a6272b64
PiachkoAliaksey <alek17y@mail.ru>
2023-01-07 16:48:35 +0300

    refactor: clean code

--- src/components/ListOfPeople.tsx
+++ src/components/ListOfPeople.tsx
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-interface IListOFPeople{
-  list:{
-    id:string,
-    name:string,
-    img:string
+interface IListOFPeople {
+  list: {
+    id: string,
+    name: string,
+    img: string
 
   }[]
 }
 
-const ListOfPeople:React.FC<IListOFPeople> = (props:IListOFPeople) => {
+const ListOfPeople: React.FC<IListOFPeople> = (props: IListOFPeople) => {
 
   return (
     <>
-    <ul className='listOfElements'>
-      {props.list.map((element:{id:string,name:string,img:string})=> <li key ={element.id} className='elementOfList'>
-        <Link className='linkOfElementList' to = {`/people/${element.id}`}>
-          <img className='imgOfElement' src = {element.img} alt = {element.name}/>
-          <p>{element.name}</p>
+      <ul className='listOfElements'>
+        {props.list.map((element: { id: string, name: string, img: string }) => <li key={element.id} className='elementOfList'>
+          <Link className='linkOfElementList' to={`/people/${element.id}`}>
+            <img className='imgOfElement' src={element.img} alt={element.name} />
+            <p>{element.name}</p>
           </Link>
         </li>)}
-    </ul>
+      </ul>
     </>
   )
 
