c984888b4b567130b50ca3869c26f8d6b01b90cc
PiachkoAliaksey <alek17y@mail.ru>
2023-01-07 16:44:58 +0300

    add: UI component LoadingPerson

---
+++ src/components/UI/UiLoadingPerson/UiLoadingPerson.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+const UiLoadingPerson = () => {
+
+  return (
+    <h2 className='block-load-movies'>Loading...</h2>
+  )
+
+}
+
+export default UiLoadingPerson;
