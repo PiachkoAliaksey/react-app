9f0b4ee5761c567d10c44e75a745787a76f5ec15
PiachkoAliaksey <alek17y@mail.ru>
2023-01-07 16:49:01 +0300

    refactor: clean code

--- src/components/BtnPagination.tsx
+++ src/components/BtnPagination.tsx
@@ -1,32 +1,22 @@
 import React, { Component } from 'react';
-import{Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import UiButton from './UI/UiButton';
 
 interface IBtnPagination {
-  setCurrentPagePrev:React.MouseEventHandler<HTMLButtonElement> | undefined
-  numberOfPage:number,
-  setCurrentPageNext:React.MouseEventHandler<HTMLButtonElement> | undefined,
-  prevPage:string|null,
-  nextPage:string|null
+  setCurrentPagePrev: React.MouseEventHandler<HTMLButtonElement> | undefined
+  numberOfPage: number,
+  setCurrentPageNext: React.MouseEventHandler<HTMLButtonElement> | undefined,
+  prevPage: string | null,
+  nextPage: string | null
 }
-const BtnPagination:React.FC<IBtnPagination> = ({setCurrentPagePrev,numberOfPage,setCurrentPageNext,prevPage,nextPage}) => {
+const BtnPagination: React.FC<IBtnPagination> = ({ setCurrentPagePrev, numberOfPage, setCurrentPageNext, prevPage, nextPage }) => {
 
   return (
 
-    <div className = 'block-of-btn'>
-    {/* <div className='block-of-btn'>
-      <button
-     className='btn-handler-page'
-     onClick={setCurrentPagePrev}
-     disabled = {!prevPage}>
-      Previous
-      </button> */}
-      <UiButton text = 'Previous' setCurrentPage={setCurrentPagePrev} setDisable = {prevPage} theme = {'dark'} />
-
-     <p className='block-number-of-page'>{numberOfPage}</p>
-     <UiButton text = 'Next' setCurrentPage={setCurrentPageNext} setDisable = {nextPage} theme = {'dark'}/>
-
-     {/* <button className='btn-handler-page' onClick={setCurrentPageNext} disabled = {!nextPage}>Next</button> */}
+    <div className='block-of-btn'>
+      <UiButton text='Previous' setCurrentPage={setCurrentPagePrev} setDisable={prevPage} theme={'dark'} />
+      <p className='block-number-of-page'>{numberOfPage}</p>
+      <UiButton text='Next' setCurrentPage={setCurrentPageNext} setDisable={nextPage} theme={'dark'} />
     </div>
   )
 
