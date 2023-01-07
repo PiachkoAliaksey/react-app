3f761a9e11419714efd2f4098569f07905ba455a
PiachkoAliaksey <alek17y@mail.ru>
2023-01-07 16:48:21 +0300

    refactor: clean code

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import{NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import UiButton from './UI/UiButton';
 
 const Navigation = () => {
@@ -9,10 +9,10 @@ const Navigation = () => {
       <nav className='navbar'>
         <h1>STAR WARS</h1>
         <ul className='navigation-list'>
-          <li className='nav-item'><NavLink to="/">Home</NavLink></li>
+          <li className='nav-item'><NavLink to="/" >Home</NavLink></li>
           <li className='nav-item' ><NavLink to="/people">People</NavLink></li>
         </ul>
-        <img className='img-star' src='../assets/img/star.png' alt = 'star'/>
+        <img className='img-star' src='../assets/img/star.png' alt='star' />
       </nav>
     </>
   )
