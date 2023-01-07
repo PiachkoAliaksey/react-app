4d4fe801810bbf80fb37c425bdd9618c6ac339f7
PiachkoAliaksey <alek17y@mail.ru>
2023-01-07 16:45:37 +0300

    add: UI component Button

--- src/components/UI/UiButton.tsx
+++ src/components/UI/UiButton.tsx
@@ -3,19 +3,19 @@ import cn from 'classnames';
 import './UiButton.css';
 
 interface IUiButton {
-  setCurrentPage:React.MouseEventHandler<HTMLButtonElement> | undefined,
-  text:string,
-  setDisable:string|null|boolean,
-  theme:string,
+  setCurrentPage: React.MouseEventHandler<HTMLButtonElement> | undefined,
+  text: string,
+  setDisable: string | null | boolean,
+  theme: string,
 }
 
-const UiButton:React.FC<IUiButton> = ({setCurrentPage,text,setDisable, theme = 'dark'}) => {
+const UiButton: React.FC<IUiButton> = ({ setCurrentPage, text, setDisable, theme = 'dark' }) => {
 
   return (
     <button
-     className={cn('btn-handler-page',theme)}
-     onClick={setCurrentPage}
-     disabled = {!setDisable}>
+      className={cn('btn-handler-page', theme)}
+      onClick={setCurrentPage}
+      disabled={!setDisable}>
       {text}
     </button>
   )
