57fffc12d3975c578d2f5f028e037b9a08ab69f6
PiachkoAliaksey <alek17y@mail.ru>
2023-01-07 16:46:13 +0300

    add: storyBook button

--- src/components/UI/UiButton.stories.tsx
+++ src/components/UI/UiButton.stories.tsx
@@ -3,35 +3,35 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import UiButton from "./UiButton";
 
 export default {
-  title:'Ui-kit/UiButton',
+  title: 'Ui-kit/UiButton',
   component: UiButton
 } as ComponentMeta<typeof UiButton>
 
 
-const Template:ComponentStory<typeof UiButton> = (args) => <UiButton {...args} />;
+const Template: ComponentStory<typeof UiButton> = (args) => <UiButton {...args} />;
 
 const props = {
-  setCurrentPage:()=>console.log('Hello'),
-  text:'Hello',
-  setDisable:true,
-  theme:'dark',
+  setCurrentPage: () => console.log('Hello'),
+  text: 'Hello',
+  setDisable: true,
+  theme: 'dark',
 }
 
 
 export const Dark = Template.bind({});
 Dark.args = {
-   ...props,
-   theme:'dark'
+  ...props,
+  theme: 'dark'
 };
 
 export const Light = Template.bind({});
 Light.args = {
-   ...props,
-   theme:'light'
+  ...props,
+  theme: 'light'
 };
 
 export const Disabled = Template.bind({});
 Disabled.args = {
-   ...props,
-   setDisable:false
+  ...props,
+  setDisable: false
 };
