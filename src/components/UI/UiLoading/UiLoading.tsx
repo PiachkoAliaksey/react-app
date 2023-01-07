61645c8f5bce88d4cde935bb47547854aa246109
PiachkoAliaksey <alek17y@mail.ru>
2023-01-07 16:45:23 +0300

    add: UI component Loading

---
+++ src/components/UI/UiLoading/UiLoading.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+const UiLoading = () => {
+
+  return (
+    <div className='block-load-data'><img className='loading-gif' /></div>
+  )
+
+}
+
+export default UiLoading;
