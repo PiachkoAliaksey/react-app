02b05b029a28c0cbef77a1634314545232394482
PiachkoAliaksey <alek17y@mail.ru>
2023-01-09 20:38:29 +0300

    refactor: hook UseEffect and useSelector

--- src/pages/FavoritePage.tsx
+++ src/pages/FavoritePage.tsx
@@ -1,13 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
+import ListOfPeople from '../components/ListOfPeople';
+
+interface IRootState {
+  name: 'string'
+}
+
 const FavoritePage = () => {
-  const storeData = useSelector((state:{favoriteReducer:{}}) => state.favoriteReducer);
-  console.log(storeData);
+  const [people, setPeople] = useState<never | {
+    id: string;
+    name: any;
+    img: any;
+  }[]>([])
+  const storeData = useSelector((state: IRootState) => {
+    return state
+  });
+
+  useEffect(() => {
+    const arrOfStoreData = Object.entries(storeData);
+    if (arrOfStoreData.length) {
+      const resArrOfStoreData = arrOfStoreData.map((value) => {
+        return {
+          id: value[0],
+          name: value[1].name,
+          img: value[1].img
+        }
+      })
+
+      setPeople(resArrOfStoreData)
+    }
+
+  }, [])
+
+
 
   return (
     <div>
-      <h2>FavoritePage</h2>
+      <h2 className='header-favorite-page'>Favorite Page</h2>
+      {people.length?<ListOfPeople list={people} />:<h2 className='header-favorite-page'>Nothing favorites peoples</h2>}
+
     </div>
   )
 
