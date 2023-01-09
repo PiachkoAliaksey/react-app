dedab688a172cf2284661e6eafad8eddde1e8dad
PiachkoAliaksey <alek17y@mail.ru>
2023-01-09 20:45:53 +0300

    refactor: functions setPersonToFavorite and removePersonFromFavorite

--- src/store/actions/index.tsx
+++ src/store/actions/index.tsx
@@ -1,14 +1,23 @@
-import { ADD_PERSON_TO_FAVORITE,REMOVE_PERSON_FROM_FAVORITE } from "store/constant/actionTypes"
+import { ADD_PERSON_TO_FAVORITE,REMOVE_PERSON_FROM_FAVORITE } from "../constant/actionTypes"
+
+
+interface IActionTypes {
+  [personId:string]:{
+    name:string,
+    img:string
+  }
+}
+export const setPersonToFavorite = (person:IActionTypes) => {
+  return {
+    type: ADD_PERSON_TO_FAVORITE,
+    payload: person
+  }
+}
 
 
 
-export const setPersonToFavorite = ():{type:string,payload:string} => ({
-  type: ADD_PERSON_TO_FAVORITE,
-  payload: ''
-})
-
-export const removePersonFromFavorite = () => ({
+export const removePersonFromFavorite = (personId:string) => ({
   type: REMOVE_PERSON_FROM_FAVORITE,
-  payload: ''
+  payload: personId
 })
 
