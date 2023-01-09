2b3dd6813deec8bbe179145baa0ac613b9734551
PiachkoAliaksey <alek17y@mail.ru>
2023-01-09 20:36:45 +0300

    refactor: dispatch hook of react-redux and element button

--- src/components/PersonPage/PersonImage/PersonPhoto.tsx
+++ src/components/PersonPage/PersonImage/PersonPhoto.tsx
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import { useDispatch } from 'react-redux';
+import { setPersonToFavorite, removePersonFromFavorite } from '../../../store/actions/index'
+
 
 interface IPersonPhoto {
-  imgPerson:string,
-  personName:string
+  setPersonFavorite: React.Dispatch<React.SetStateAction<boolean>>,
+  personFavorite: boolean,
+  personId: string,
+  imgPerson: string,
+  personName: string
 }
-const PersonPhoto: React.FC<IPersonPhoto> = ({imgPerson,personName}) => {
+const PersonPhoto: React.FC<IPersonPhoto> = ({ personFavorite, setPersonFavorite, personId, imgPerson, personName }) => {
+  const dispatchColorOfStar = useDispatch();
+
+  const dispatchFavorite = () => {
+    if (personFavorite) {
+      dispatchColorOfStar(removePersonFromFavorite(personId));
+      setPersonFavorite(false);
+    } else {
+      dispatchColorOfStar(setPersonToFavorite({ [personId]: { 'name': personName, 'img': imgPerson } }));
+      setPersonFavorite(true);
+    }
+  }
 
   return (
-    <>
-     <img className='imgOfPerson' src = {imgPerson} alt = {personName}/>
-    </>
+    <div className='block-of-image-person'>
+      <img className='imgOfPerson' src={imgPerson} alt={personName} />
+      <button className='btn-add-to-favorite' onClick={dispatchFavorite}>{personFavorite ? <img className='img-favorite' src='../../assets/img/favorite-fill.svg' alt='star-active' /> : <img className='img-favorite' src='../../assets/img/favorite.svg' alt='star-inactive' />}</button>
+    </div>
   )
 
 }
