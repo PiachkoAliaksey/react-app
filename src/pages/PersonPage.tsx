48cbf49503bab42357391fafa122e5dce5a15548
PiachkoAliaksey <alek17y@mail.ru>
2023-01-09 20:40:28 +0300

    refactor: hook useSelector and useEffect

--- src/pages/PersonPage.tsx
+++ src/pages/PersonPage.tsx
@@ -1,5 +1,6 @@
 import React, { Component, useEffect, useState, Suspense } from 'react';
 import { useParams } from "react-router-dom";
+import { useSelector } from 'react-redux';
 import { getApiRes, getCurrentRequest } from '../utils/network';
 import { rootPeople } from '../constants/api';
 import { withErrorApi } from '../hoc-helpers/withErrorApi';
@@ -19,21 +20,33 @@ interface IPersonPage {
   setErrorApi: React.Dispatch<React.SetStateAction<boolean>>
 }
 
+interface IRootState {
+  [num: string]: { name: string, img: string }
+}
+
 const PersonPage: React.FC<IPersonPage> = ({ setErrorApi }) => {
   const { id } = useParams();
+  const storeData = useSelector((state: IRootState) => {
+    return state
+  });
 
+  const [personId, setPersonId] = useState('');
   const [person, setPerson] = useState([{ title: '', data: '' }]);
   const [personName, setPersonName] = useState('');
   const [imgPerson, setImgPerson] = useState('');
   const [personMovies, setPersonMovies] = useState<Array<{ title: string, episode_id: number }>>([]);
   const [isLoadingMovies, setLoadingMovies] = useState(false);
+  const [personFavorite, setPersonFavorite] = useState(false)
 
   useEffect(() => {
     const fetchData = async () => {
       setLoadingMovies(true);
-      const data = await getApiRes(`${rootPeople}/${id}/`)
+      const data = await getApiRes(`${rootPeople}/${id}/`);
+
+      storeData[id!] ? setPersonFavorite(true) : setPersonFavorite(false);
+      setPersonId(id!);
+
       if (data) {
-        console.log(data);
         const image = getPeopleImg(id!);
         setImgPerson(image)
         setPersonName(data.name);
@@ -44,11 +57,9 @@ const PersonPage: React.FC<IPersonPage> = ({ setErrorApi }) => {
           { title: 'Eye color', data: data.eye_color },
           { title: 'Birth year', data: data.birth_year },
         ]);
-        //res.films
-        //console.log(data.films)
+
         if (data.films.length) {
           const movies = await getCurrentRequest(data.films);
-          console.log(movies);
           setLoadingMovies(false);
           setPersonMovies(movies);
         }
@@ -58,8 +69,6 @@ const PersonPage: React.FC<IPersonPage> = ({ setErrorApi }) => {
       }
     }
     fetchData();
-
-
   }, [])
 
   return (
@@ -68,9 +77,9 @@ const PersonPage: React.FC<IPersonPage> = ({ setErrorApi }) => {
         <PersonLinkBack />
         <h2 className='name-of-person'>Name: {personName}</h2>
         <div className='block-person-properties'>
-          <PersonPhoto imgPerson={imgPerson} personName={personName} />
+          <PersonPhoto setPersonFavorite={setPersonFavorite} personFavorite={personFavorite} personId={personId} imgPerson={imgPerson} personName={personName} />
           {person && (<PersonInfo arrPerson={person} />)}
-          {isLoadingMovies?<UiLoadingPerson/>:(personMovies && <PersonMovies personMovies={personMovies} />)}
+          {isLoadingMovies ? <UiLoadingPerson /> : (personMovies && <PersonMovies personMovies={personMovies} />)}
         </div>
 
       </div>
