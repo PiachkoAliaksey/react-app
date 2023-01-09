a9be62ef47e73d3c28e2925decad82b3d1a21e25
PiachkoAliaksey <alek17y@mail.ru>
2023-01-09 20:44:24 +0300

    add: function setLocalStorage, component composeWithDevTools of redux-devtools-extension, applyMiddleware of redux

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,5 +1,13 @@
-import { legacy_createStore as createStore} from 'redux';
+import { legacy_createStore as createStore, applyMiddleware} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
 import favoriteReducer from './reducers/favoriteReducer';
-const store = createStore(favoriteReducer);
+import { setLocalStorage } from '../utils/localStorage';
+
+const store = createStore(favoriteReducer,composeWithDevTools(applyMiddleware(thunk)));
+store.subscribe(()=>{
+  setLocalStorage('store',store.getState());
+});
 
 export default store;
