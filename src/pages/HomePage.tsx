684d526731e52a026dd2e14c3ae9a4da48eb20dc
PiachkoAliaksey <alek17y@mail.ru>
2023-01-14 17:32:04 +0300

    refactor: add functional component ChooseSide in Home Page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import ChooseSide from '../components/ChooseSide';
+
+
 const HomePage = () => {
 
   return (
     <>
       <main className='home-page'>
-        <h2>Welcome to Universe of Star Wars</h2>
+        <h2 className='header-main-page'>Welcome to Universe of Star Wars</h2>
+        <ChooseSide/>
       </main>
     </>
   )
