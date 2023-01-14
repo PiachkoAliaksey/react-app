6326fbad8a0c342470a93e7181d98a3dba8fbb1a
PiachkoAliaksey <alek17y@mail.ru>
2023-01-14 17:39:14 +0300

    refactor: main index.tss (add Provider(Redux) and functional component ThemeProvider)

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import store from "./store/store";
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
+import {ThemeProvider} from "./context/ThemeProvider";
 
-ReactDOM.render(<Provider store = {store} ><App /></Provider>, document.getElementById("root") as HTMLElement);
+ReactDOM.render(<Provider store={store} ><ThemeProvider><App /></ThemeProvider></Provider>, document.getElementById("root") as HTMLElement);
